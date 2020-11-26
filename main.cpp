259e383a536e0c02efa287d2c26e384b95b0b130
Nurio Fernández <personal@nurio.me>
2020-11-26 21:54:45 +0100

    Print a message every time some data get change

--- main.cpp
+++ main.cpp
@@ -8,11 +8,18 @@ int main() {
     gameResources = new GameResources();
     printf("Successfully injected! \\@.@/\n");
 
-    float money = gameResources->GetMoney();
-    printf("You have $%f.\n", money);
+    float money = -1;
+    int population = -1;
 
-    int population = gameResources->GetPopulation();
-    printf("You have a population of %d tropicans.\n", population);
+    while (true) {
+        Sleep(500);
+        float tmpMoney = money;
+        int tmpPopulation = population;
+        money = gameResources->GetMoney();
+        population = gameResources->GetPopulation();
 
+        if (money != tmpMoney) printf("You have $%f.\n", money);
+        if (population != tmpPopulation) printf("You have a population of %d tropicans.\n", population);
+    }
     return 0;
 }
