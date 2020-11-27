f8caf6866e1bb2e099d26691048921e69a4376fc
Nurio Fernández <personal@nurio.me>
2020-11-28 00:43:33 +0100

    Prints a message each time the year changes

--- main.cpp
+++ main.cpp
@@ -10,16 +10,20 @@ int main() {
 
     float money = -1;
     int population = -1;
+    int year = -1;
 
     while (true) {
         Sleep(500);
         float tmpMoney = money;
         int tmpPopulation = population;
+        int tmpYear = year;
         money = gameResources->GetMoney();
         population = gameResources->GetPopulation();
+        year = gameResources->GetYear();
 
         if (money != tmpMoney) printf("You have $%f.\n", money);
         if (population != tmpPopulation) printf("You have a population of %d tropicans.\n", population);
+        if (year != tmpYear) printf("You are in the year %d.\n", year);
     }
     return 0;
 }
