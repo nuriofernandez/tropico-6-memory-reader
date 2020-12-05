f753749b3b79286d1dc0f34e0f175e55823ca608
Nurio Fernández <personal@nurio.me>
2020-12-05 03:05:13 +0100

    Added support changing detection to main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -10,6 +10,7 @@ int main() {
 
     float money = -1;
     int population = -1;
+    float support = -1;
     int year = -1;
     int month = -1;
     bool paused;
@@ -18,17 +19,20 @@ int main() {
         Sleep(500);
         float tmpMoney = money;
         int tmpPopulation = population;
+        float tmpSupport = support;
         int tmpYear = year;
         int tmpMonth = month;
         int tmpPaused = paused;
         money = gameResources->GetMoney();
         population = gameResources->GetPopulation();
+        support = gameResources->GetSupport();
         year = gameResources->GetYear();
         month = gameResources->GetMonth();
         paused = gameResources->IsPaused();
 
         if (money != tmpMoney) printf("You have $%f.\n", money);
         if (population != tmpPopulation) printf("You have a population of %d tropicans.\n", population);
+        if (support != tmpSupport) printf("Your current support is %f.\n", support);
         if (year != tmpYear) printf("You are in the year %d.\n", year);
         if (month != tmpMonth) printf("You are in the month %d.\n", month);
         if (paused != tmpPaused) printf("You game %s paused.\n", paused ? "is" : "is not");
