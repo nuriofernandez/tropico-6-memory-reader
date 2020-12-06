30b2a3cc94ad5463dd9e67331dec6709f831579d
Nurio Fernández <personal@nurio.me>
2020-12-06 01:53:01 +0100

    Added unemployment changes detection message

--- src/main.cpp
+++ src/main.cpp
@@ -14,6 +14,7 @@ int main() {
     int year = -1;
     int month = -1;
     bool paused;
+    int unemployment = -1;
 
     while (true) {
         Sleep(500);
@@ -23,6 +24,7 @@ int main() {
         int tmpYear = year;
         int tmpMonth = month;
         bool tmpPaused = paused;
+        int tmpUnemployment = unemployment;
 
         money = gameResources->GetMoney();
         population = gameResources->GetPopulation();
@@ -30,6 +32,7 @@ int main() {
         year = gameResources->GetYear();
         month = gameResources->GetMonth();
         paused = gameResources->IsPaused();
+        unemployment = gameResources->GetUnemployedCitizens();
 
         if (money != tmpMoney) printf("You have $%f.\n", money);
         if (population != tmpPopulation) printf("You have a population of %d tropicans.\n", population);
@@ -37,6 +40,7 @@ int main() {
         if (year != tmpYear) printf("You are in the year %d.\n", year);
         if (month != tmpMonth) printf("You are in the month %d.\n", month);
         if (paused != tmpPaused) printf("You game %s paused.\n", paused ? "is" : "is not");
+        if (unemployment != tmpUnemployment) printf("You have %d unemployed tropicans.\n", unemployment);
     }
     return 0;
 }
